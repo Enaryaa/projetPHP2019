2799547cca91767b24a0891b89e3943d0d18ade2
Lechartier <lecharti@localhost.localdomain>
2019-06-10 10:16:35 +0000

    maj

--- application/views/header.php
+++ application/views/header.php
@@ -35,6 +35,9 @@
           <a class="dropdown-item" href="<?php echo site_url('formulaire'); ?>">Créer un formulaire</a>
           <a class="dropdown-item" href="#">Gérer les formulaires</a>
         </div>
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Formulaire
+        </a>
         
       </li>
 
